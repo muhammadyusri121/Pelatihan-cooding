c4ad7139a0253ba62ac0d2c43b6409d00a612d62
muhammadyusri121 <135690839+muhammadyusri121@users.noreply.github.com>
2023-09-02 10:42:42 +0700

    first commit

---
+++ python.py
@@ -0,0 +1,21 @@
+# appel = 100
+# print(appel)
+# print("cek python")
+# nama_apel = "apel malang"
+# apel_benar = True
+# apel_salah = False
+
+# tuple_jumlah = (2,4,53, "situ")
+# print(tuple_jumlah)
+# apel_dictionary = {}
+# print= apel_dictionary
+
+a = 20030
+b = 3536
+print(a+b)
+
+print(8/2)
+
+x = 324
+y = 324
+print(x+y)
