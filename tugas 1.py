fcc8ab077ccbf741afba5864dd0cd36c930ccbb8
muhammadyusri121 <135690839+muhammadyusri121@users.noreply.github.com>
2023-09-04 17:27:33 +0700

    tugas menghitung luas balok menggunakan python

---
+++ tugas 1.py
@@ -0,0 +1,12 @@
+print("selamat datang di penghitung luas balok sederhana")
+print("silahkan masukkan data anda di bawah ini")
+panjang = int(input("panjang perseginya berapa bang = "))
+lebar = int(input("lebar perseginya juga jangan lupa = "))
+tinggi = int(input("tinggi peseginya juga yaaa = "))
+
+luas_persegi = panjang*lebar*tinggi
+
+# print(panjang)
+# print(lebar)
+# print(tinggi)
+print("volume balok adalah", luas_persegi, "cm")
